a985e0cecf6f59b518c4b261a59fa4122fd29a92
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-04 12:49:01 +0100

    Fix broken removal script.

--- jdk_prerm.sh
+++ jdk_prerm.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/sh
 
-update-alternatives --remove "/usr/bin/java" "java" "/usr/lib/jvm/jdk-21.0.1/bin/java" 0
-update-alternatives --remove "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk-21.0.1/bin/javac" 0
-update-alternatives --remove "/usr/bin/jlink" "jlink" "/usr/lib/jvm/jdk-21.0.1/bin/jlink" 0
-update-alternatives --remove "/usr/bin/jshell" "jshell" "/usr/lib/jvm/jdk-21.0.1/bin/jshell" 0
-update-alternatives --remove "/usr/bin/javap" "javap" "/usr/lib/jvm/jdk-21.0.1/bin/javap" 0
-update-alternatives --remove "/usr/bin/javadoc" "javadoc" "/usr/lib/jvm/jdk-21.0.1/bin/javadoc" 0
-update-alternatives --remove "/usr/bin/jar" "jar" "/usr/lib/jvm/jdk-21.0.1/bin/jar" 0
-update-alternatives --remove "/usr/bin/jmod" "jmod" "/usr/lib/jvm/jdk-21.0.1/bin/jmod" 0
-update-alternatives --remove "/usr/bin/jdeps" "jdeps" "/usr/lib/jvm/jdk-21.0.1/bin/jdeps" 0
-update-alternatives --remove "/usr/bin/jdeprscan" "jdeprscan" "/usr/lib/jvm/jdk-21.0.1/bin/jdeprscan" 0
-update-alternatives --remove "/usr/bin/jarsigner" "jarsigner" "/usr/lib/jvm/jdk-21.0.1/bin/jarsigner" 0
-update-alternatives --remove "/usr/bin/keytool" "keytool" "/usr/lib/jvm/jdk-21.0.1/bin/keytool" 0
-update-alternatives --remove "/usr/bin/jconsole" "jconsole" "/usr/lib/jvm/jdk-21.0.1/bin/jconsole" 0
-update-alternatives --remove "/usr/bin/jdb" "jdb" "/usr/lib/jvm/jdk-21.0.1/bin/jdb" 0
-update-alternatives --remove "/usr/bin/jcmd" "jcmd" "/usr/lib/jvm/jdk-21.0.1/bin/jcmd" 0
+update-alternatives --remove "java" "/usr/lib/jvm/jdk-23/bin/java"
+update-alternatives --remove "javac" "/usr/lib/jvm/jdk-23/bin/javac"
+update-alternatives --remove "jlink" "/usr/lib/jvm/jdk-23/bin/jlink"
+update-alternatives --remove "jshell" "/usr/lib/jvm/jdk-23/bin/jshell"
+update-alternatives --remove "javap" "/usr/lib/jvm/jdk-23/bin/javap"
+update-alternatives --remove "javadoc" "/usr/lib/jvm/jdk-23/bin/javadoc"
+update-alternatives --remove "jar" "/usr/lib/jvm/jdk-23/bin/jar"
+update-alternatives --remove "jmod" "/usr/lib/jvm/jdk-23/bin/jmod"
+update-alternatives --remove "jdeps" "/usr/lib/jvm/jdk-23/bin/jdeps"
+update-alternatives --remove "jdeprscan" "/usr/lib/jvm/jdk-23/bin/jdeprscan"
+update-alternatives --remove "jarsigner" "/usr/lib/jvm/jdk-23/bin/jarsigner"
+update-alternatives --remove "keytool" "/usr/lib/jvm/jdk-23/bin/keytool"
+update-alternatives --remove "jconsole" "/usr/lib/jvm/jdk-23/bin/jconsole"
+update-alternatives --remove "jdb" "/usr/lib/jvm/jdk-23/bin/jdb"
+update-alternatives --remove "jcmd" "/usr/lib/jvm/jdk-23/bin/jcmd"
 
