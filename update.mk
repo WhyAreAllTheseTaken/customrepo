4995bd5784af455f848170fd1fd7d3275773128e
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-04 12:20:22 +0100

    Put sources in the correct place.

--- update.mk
+++ update.mk
@@ -9,7 +9,7 @@ $(UPDATER_PACKAGE).deb:
 	mkdir -p $(UPDATER_PACKAGE)/etc/apt/source.list.d/
 	cp ./custom_repo_build.sh $(UPDATER_PACKAGE)/usr/sbin/custom_repo_build
 	chmod +x $(UPDATER_PACKAGE)/usr/sbin/custom_repo_build
-	cp ./custom_repo.list $(UPDATER_PACKAGE)/etc/apt/source.list.d/
+	cp ./custom_repo.list $(UPDATER_PACKAGE)/etc/apt/sources.list.d/
 	git clone --depth 1 "https://github.com/WhyAreAllTheseTaken/customrepo.git" $(UPDATER_PACKAGE)/usr/customrepo
 	mkdir -p $(UPDATER_PACKAGE)/DEBIAN
 	cp update_control $(UPDATER_PACKAGE)/DEBIAN/control
