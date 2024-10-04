e317cd75c6541f75199afb896d927ad51f5966b0
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-04 15:13:50 +0100

    Fix repo URL.

--- wings_plasma.mk
+++ wings_plasma.mk
@@ -4,7 +4,7 @@ WINGS_PLASMA_PACKAGE = wings-plasma-themes_$(WINGS_PLASMA_VERSION)-$(WINGS_PLASM
 
 $(WINGS_PLASMA_PACKAGE).deb:
 	rm -rfv ./Wings-Plasma-Themes
-	git clone --depth 1 https://github.com/L4ki/Wings-Plasma-Themes.
+	git clone --depth 1 https://github.com/L4ki/Wings-Plasma-Themes.git
 	mkdir -p $(WINGS_PLASMA_PACKAGE)/usr/share/icons/
 	cp -rv ./Wings-Plasma-Themes/Wings\ Icons\ Themes/* $(WINGS_PLASMA_PACKAGE)/usr/share/icons/
 	mkdir -p $(WINGS_PLASMA_PACKAGE)/usr/share/backgrounds/
