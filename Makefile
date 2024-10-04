4cdb2434ab0e0eac39fba20dcc49e0a89484171c
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-04 14:54:00 +0100

    Add i3blocks-contrib.

--- Makefile
+++ Makefile
@@ -13,5 +13,6 @@ updater: $(UPDATER_PACKAGE).deb
 install:
 	apt install ./$(UPDATER_PACKAGE).deb
 
-packages: $(CPMV_PACKAGE).deb $(JDK_PACKAGE).deb $(LTSA_PACKAGE).deb nvim-linux64.deb $(PACKER_PACKAGE).deb $(UPDATER_PACKAGE).deb $(FRITURE_PACKAGE).deb
+packages: $(CPMV_PACKAGE).deb $(JDK_PACKAGE).deb $(LTSA_PACKAGE).deb nvim-linux64.deb $(PACKER_PACKAGE).deb \
+	$(UPDATER_PACKAGE).deb $(FRITURE_PACKAGE).deb $(I3_BLOCKS_PACKAGE).deb
 
---
+++ i3.mk
@@ -0,0 +1,13 @@
+I3_BLOCKS_VERSION = 2.0.0
+I3_BLOCKS_REVISION = 1
+I3_BLOCKS_PACKAGE = i3blocks-contrib_$(I3_BLOCKS_VERSION)-$(I3_BLOCKS_REVISION)
+
+$(I3_BLOCKS_PACKAGE).deb:
+	rm -rfv ./i3blocks-contrib
+	git clone --depth 1 --branch v2.0.0 https://github.com/vivien/i3blocks-contrib.git
+	mkdir -p $(I3_BLOCKS_PACKAGE)/etc/skel/.config/i3block/
+	cp -rv ./i3blocks-contrib/* $(I3_BLOCKS_PACKAGE)/etc/skel/.config/i3blocks
+	mkdir -p $(I3_BLOCKS_PACKAGE)/DEBIAN
+	cp i3_blocks_control $(I3_BLOCKS_PACKAGE)/DEBIAN/control
+	dpkg-deb --build $(I3_BLOCKS_PACKAGE)
+
