2f0a6e6eb8f1b569af164496f3b074c7ce9aeb00
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-04 16:57:11 +0100

    Maybe fix friture install.

--- friture_postinst.sh
+++ friture_postinst.sh
@@ -3,6 +3,6 @@
 cd /usr/share/friture
 virtualenv -p /usr/bin/python3.11 buildenv
 source ./buildenv/bin/activate
-pip3.11 install .[dev]
+pip3.11 install .
 python3.11 setup.py build_ext --inplace
 
