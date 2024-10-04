42d97ddbcdabc4a6869d6dcd1ace23019436e468
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-04 12:29:36 +0100

    Add multiversion support.

--- custom_repo_build.sh
+++ custom_repo_build.sh
@@ -2,5 +2,5 @@
 
 cd /usr/customrepo && sudo -u customrepo -- git pull origin master
 cd /usr/customrepo && sudo -u customrepo -- make repo
-cd /usr/customrepo/repo && sudo -u customrepo -- dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
+cd /usr/customrepo/repo && sudo -u customrepo -- dpkg-scanpackages -m . /dev/null | gzip -9c > Packages.gz
 
