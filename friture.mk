ad728a734239953040b3b1e375547c361a06cd23
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-04 14:29:34 +0100

    Remove apt line.

--- friture.mk
+++ friture.mk
@@ -3,7 +3,6 @@ FRITURE_REVISION = 1
 FRITURE_PACKAGE = friture_$(FRITURE_VERSION)-$(FRITURE_REVISION)
 
 $(FRITURE_PACKAGE).deb:
-	sudo apt install git libportaudio2 python3.11-dev virtualenv
 	git clone --depth 1 --branch v0.51 https://github.com/tlecomte/friture.git
 	mkdir -p $(FRITURE_PACKAGE)/usr/share/
 	cp -rv ./friture $(FRITURE_PACKAGE)/usr/share
