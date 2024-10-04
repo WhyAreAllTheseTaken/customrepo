d46460726f5105c16fae2d718b50e51f3d434d78
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-04 18:30:04 +0100

    Get i3lock color building.

--- i3lock-color_postinst.sh
+++ i3lock-color_postinst.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 
-cd /usr/share/i3lock-color-build/build
-make install
-LICENSE='/usr/share/licenses/i3lock-color/LICENSE'
-sudo install -Dm644 ../LICENSE "$LICENSE"
+cd /usr/share/i3lock-color-build/
+./install_i3lock-color.sh
 
--- i3lock.mk
+++ i3lock.mk
@@ -1,14 +1,10 @@
 I3LOCK_COLOR_VERSION = 2.13.c.5
-I3LOCK_COLOR_REVISION = 5
+I3LOCK_COLOR_REVISION = 6
 I3LOCK_COLOR_PACKAGE = i3lock-color_$(I3LOCK_COLOR_VERSION)-$(I3LOCK_COLOR_REVISION)
 
 $(I3LOCK_COLOR_PACKAGE).deb:
 	rm -rf i3lock-color
 	git clone --depth 1 --branch $(I3LOCK_COLOR_VERSION) https://github.com/Raymo111/i3lock-color.git
-	cd i3lock-color && autoreconf -fiv
-	mkdir -p i3lock-color/build
-	cd i3lock-color/build && ../configure --prefix=/usr --sysconfdir=/etc
-	cd i3lock-color/build && make
 	mkdir -p $(I3LOCK_COLOR_PACKAGE)/usr/share/i3lock-color-build
 	cp -r i3lock-color/* $(I3LOCK_COLOR_PACKAGE)/usr/share/i3lock-color-build
 	mkdir -p $(I3LOCK_COLOR_PACKAGE)/DEBIAN
