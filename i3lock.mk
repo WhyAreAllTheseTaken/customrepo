1a9b68f789e448d0f83c91ff87de6d17e5837270
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-04 18:01:40 +0100

    Fix i3lock path

--- i3lock.mk
+++ i3lock.mk
@@ -1,6 +1,7 @@
 I3LOCK_COLOR_VERSION = 2.13.c.5
 I3LOCK_COLOR_REVISION = 2
 I3LOCK_COLOR_PACKAGE = i3lock-color_$(I3LOCK_COLOR_VERSION)-$(I3LOCK_COLOR_REVISION)
+I3LOCK_COLOR_PACKAGE_REAL = $(realpath ./$(I3LOCK_COLOR_PACKAGE))
 
 $(I3LOCK_COLOR_PACKAGE).deb:
 	rm -rf nnn
@@ -9,7 +10,7 @@ $(I3LOCK_COLOR_PACKAGE).deb:
 	mkdir -p $(I3LOCK_COLOR_PACKAGE)/usr
 	cd i3lock-color && autoreconf -fiv
 	mkdir -p i3lock-color/build
-	cd i3lock-color/build && ../configure --prefix=$(I3LOCK_COLOR_PACKAGE)/usr --sysconfdir=$(I3LOCK_COLOR_PACKAGE)/etc
+	cd i3lock-color/build && ../configure --prefix=$(I3LOCK_COLOR_PACKAGE_REAL)/usr --sysconfdir=$(I3LOCK_COLOR_PACKAGE_REAL)/etc
 	cd i3lock-color/build && make
 	cd i3lock-color/build && make install
 	mkdir -p $(I3LOCK_COLOR_PACKAGE)/DEBIAN
