1be9962f071944ce3729ae6635cd08bb69234922
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-04 17:57:08 +0100

    Fix repo not built.

--- custom_repo_build.sh
+++ custom_repo_build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/bash
 
-apt update
 cd /usr/customrepo && sudo -u customrepo -- git pull origin master
-cd /usr/customrepo && sudo -u customrepo -- make updater
+cd /usr/customrepo && sudo -u customrepo -- make build
 cd /usr/customrepo && make install
+cd /usr/customrepo && sudo -u customrepo -- make repo
 cd /usr/customrepo/repo && sudo -u customrepo -- dpkg-scanpackages -m . /dev/null | gzip -9c > Packages.gz
-apt update
 
