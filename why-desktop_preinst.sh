359a8747c80f27c1de4274ccef5720e1624ed4b5
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-06 22:43:45 +0100

    Fix why-desktop invalid argument.

--- why-desktop_preinst.sh
+++ why-desktop_preinst.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-dpkg-divert ---package why-desktop --add --rename --divert /etc/lightdm/lightdm-gtk-greeter.conf.old /etc/lightdm/lightdm-gtk-greeter.conf
+dpkg-divert --package why-desktop --add --rename --divert /etc/lightdm/lightdm-gtk-greeter.conf.old /etc/lightdm/lightdm-gtk-greeter.conf
