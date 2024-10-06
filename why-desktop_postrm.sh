759f65ce36350fe5649db7fc780395ca2bd5ba5a
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-06 22:55:18 +0100

    Switch to using mv.

--- why-desktop_postrm.sh
+++ why-desktop_postrm.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-dpkg-divert --package why-desktop --remove --rename --divert /etc/lightdm/lightdm-gtk-greeter.conf.old /etc/lightdm/lightdm-gtk-greeter.conf
+mv /etc/lightdm/lightdm-gtk-greeter.conf.old /etc/lightdm/lightdm-gtk-greeter.conf
 
