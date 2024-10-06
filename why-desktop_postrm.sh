496064f3a61ab68daa7ade126f2ed2de707ef407
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-06 22:38:29 +0100

    Fix why-desktop calls to dpkg-divert.

--- why-desktop_postrm.sh
+++ why-desktop_postrm.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-dpkg-divert --add --package why-desktop --remove --rename --divert /etc/lightdm/lightdm-gtk-greeter.conf.old /etc/lightdm/lightdm-gtk-greeter
+dpkg-divert --package why-desktop --remove --rename --divert /etc/lightdm/lightdm-gtk-greeter.conf.old /etc/lightdm/lightdm-gtk-greeter
 
--- why-desktop_preinst.sh
+++ why-desktop_preinst.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-dpkg-divert --add --package why-desktop --add --rename --divert /etc/lightdm/lightdm-gtk-greeter.conf.old /etc/lightdm/lightdm-gtk-greeter
+dpkg-divert ---package why-desktop --add --rename --divert /etc/lightdm/lightdm-gtk-greeter.conf.old /etc/lightdm/lightdm-gtk-greeter
