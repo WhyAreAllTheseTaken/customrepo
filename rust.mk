9d5afa6710cda38d05c516a42bbeb3db1ae746aa
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-06 15:52:46 +0100

    Fix rustup package.

--- rust.mk
+++ rust.mk
@@ -1,10 +1,13 @@
 RUSTUP_VERSION = 1.27.1
-RUSTUP_REVISION = 1
+RUSTUP_REVISION = 2
 RUSTUP_PACKAGE = rustup_$(RUSTUP_VERSION)-$(RUSTUP_REVISION)
 
 $(RUSTUP_PACKAGE).deb:
 	mkdir -p $(RUSTUP_PACKAGE)/DEBIAN
-	wget --show-progress -O $(RUSTUP_PACKAGE)/DEBIAN/postinst https://sh.rustup.rs
+	mkdir -p $(RUSTUP_PACKAGE)/usr/share/rustup_deb
+	wget --show-progress -O $(RUSTUP_PACKAGE)/usr/share/rustup_deb/rustup_setup.sh https://sh.rustup.rs
+	chmod +x $(RUSTUP_PACKAGE)/usr/share/rustup_deb/rustup_setup.sh
+	cp rustup_postinst.sh $(RUSTUP_PACKAGE)/DEBIAN/postinst
 	chmod +x $(RUSTUP_PACKAGE)/DEBIAN/postinst
 	cp rustup_control $(RUSTUP_PACKAGE)/DEBIAN/control
 	dpkg-deb --build $(RUSTUP_PACKAGE)
