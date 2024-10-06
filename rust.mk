7cb1858e4f7feeead8ae274ad9d0ae4d5888aa55
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-06 15:44:56 +0100

    Fix rust path again.

--- rust.mk
+++ rust.mk
@@ -4,7 +4,7 @@ RUSTUP_PACKAGE = rustup_$(RUSTUP_VERSION)-$(RUSTUP_REVISION)
 
 $(RUSTUP_PACKAGE).deb:
 	mkdir -p $(RUSTUP_PACKAGE)/DEBIAN
-	wget --show-progress -O $(OBSIDIAN_PACKAGE)/DEBIAN/postinst https://sh.rustup.rs
+	wget --show-progress -O $(RUSTUP_PACKAGE)/DEBIAN/postinst https://sh.rustup.rs
 	cp rustup_control $(RUSTUP_PACKAGE)/DEBIAN/control
 	dpkg-deb --build $(RUSTUP_PACKAGE)
 	rm -rf $(RUSTUP_PACKAGE)
