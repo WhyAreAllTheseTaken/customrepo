528be8737611c6b27ec2189960436837d6c766ad
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-06 16:35:01 +0100

    Add custom nnn build.

---
+++ start_nnn.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/zsh
+
+source ~/.zshrc
+nnn -eo
+
---
+++ why-cli.mk
@@ -0,0 +1,13 @@
+WHY_NNN_VERSION = 0.0.0
+WHY_NNN_REVISION = 1
+WHY_NNN_PACKAGE = why-nnn_$(WHY_NNN_VERSION)-$(WHY_NNN_REVISION)
+
+$(WHY_NNN_PACKAGE).deb:
+	rm -rfv $(WHY_NNN_PACKAGE)
+	mkdir -p $(WHY_NNN_PACKAGE)/usr/bin/
+	cp ./start_nnn.sh $(WHY_NNN_PACKAGE)/usr/bin/start_nnn
+	chmod +x $(WHY_NNN_PACKAGE)/usr/bin/start_nnn
+	mkdir -p $(WHY_NNN_PACKAGE)/DEBIAN
+	cp why-nnn_control $(WHY_NNN_PACKAGE)/DEBIAN/control
+	rm -rf $(WHY_NNN_PACKAGE)
+
