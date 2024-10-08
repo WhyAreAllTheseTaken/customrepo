7a45a6faa6f58aca18292cc29f62fd783c6baf54
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-08 14:55:01 +0100

    Add more information during operation.

--- custom_repo_build.sh
+++ custom_repo_build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/bash
 
+echo "Getting the latest build instructions..."
 cd /usr/customrepo && sudo -u customrepo -- git pull origin master
+echo "Checking self-update..."
 cd /usr/customrepo && sudo -u customrepo -- make -s --debug=b build
+echo "Building packages..."
 cd /usr/customrepo && sudo -u customrepo -- make -s --debug=b --keep-going packages
+echo "Updating repository. This may take some time..."
 cd /usr/customrepo && sudo -u customrepo -- dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
+echo "Custom repository has been updated. Run apt update to check for package updates."
 
