d963c9ad29cffa2591517515d51f00bbe479011b
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-08 13:42:22 +0100

    Add laravel.

---
+++ laravel.mk
@@ -0,0 +1,13 @@
+LARAVEL_VERSION = 5.9.0
+LARAVEL_REVISION = 1
+LARAVEL_PACKAGE = laravel-installer_$(LARAVEL_VERSION)-$(LARAVEL_REVISION)
+
+$(LARAVEL_PACKAGE).deb:
+	mkdir -p $(LARAVEL_PACKAGE)/DEBIAN
+	cp laravel_postinst.sh $(LARAVEL_PACKAGE)/DEBIAN/postinst
+	chmod +x $(LARAVEL_PACKAGE)/DEBIAN/postinst
+	cp laravel_control $(LARAVEL_PACKAGE)/DEBIAN/control
+	dpkg-deb --build $(LARAVEL_PACKAGE)
+	rm -rf $(LARAVEL_PACKAGE)
+
+
---
+++ laravel_prerm.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+composer global remove laravel/installer
+
+
