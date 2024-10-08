cc7e956153de4cdfe4b565ceb7e3609334b06880
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-08 14:46:22 +0100

    DOn't use multiversion repos.

--- custom_repo_build.sh
+++ custom_repo_build.sh
@@ -2,7 +2,6 @@
 
 cd /usr/customrepo && sudo -u customrepo -- git pull origin master
 cd /usr/customrepo && sudo -u customrepo -- make -s build
-cd /usr/customrepo && make install
 cd /usr/customrepo && sudo -u customrepo -- make -s --keep-going packages
-cd /usr/customrepo && sudo -u customrepo -- dpkg-scanpackages -m . /dev/null | gzip -9c > Packages.gz
+cd /usr/customrepo && sudo -u customrepo -- dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
 
