c64692b86b8a03ed3eb3f7068d4325b2e526fa4a
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-08 11:38:54 +0100

    Remove backgrounds from wings-plasma-themes.

--- wings_plasma.mk
+++ wings_plasma.mk
@@ -1,4 +1,4 @@
-WINGS_PLASMA_VERSION = 0.0.0
+WINGS_PLASMA_VERSION = 1.0.0
 WINGS_PLASMA_REVISION = 1
 WINGS_PLASMA_PACKAGE = wings-plasma-themes_$(WINGS_PLASMA_VERSION)-$(WINGS_PLASMA_REVISION)
 
@@ -7,8 +7,6 @@ $(WINGS_PLASMA_PACKAGE).deb:
 	git clone --depth 1 https://github.com/L4ki/Wings-Plasma-Themes.git
 	mkdir -p $(WINGS_PLASMA_PACKAGE)/usr/share/icons/
 	cp -rv ./Wings-Plasma-Themes/Wings\ Icons\ Themes/* $(WINGS_PLASMA_PACKAGE)/usr/share/icons/
-	mkdir -p $(WINGS_PLASMA_PACKAGE)/usr/share/backgrounds/
-	cp -rv ./Wings-Plasma-Themes/Wings\ Wallpapers/* $(WINGS_PLASMA_PACKAGE)/usr/share/backgrounds/
 	mkdir -p $(WINGS_PLASMA_PACKAGE)/DEBIAN
 	cp wings_plasma_control $(WINGS_PLASMA_PACKAGE)/DEBIAN/control
 	dpkg-deb --build $(WINGS_PLASMA_PACKAGE)
