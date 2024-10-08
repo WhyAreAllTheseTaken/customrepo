a559801a8daccdb4fbf53caf3638e3cdce3842b9
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-08 14:58:58 +0100

    Allow multiple package versions again.

--- custom_repo_build.sh
+++ custom_repo_build.sh
@@ -7,6 +7,6 @@ cd /usr/customrepo && sudo -u customrepo -- make -s --debug=b build
 echo "Building packages..."
 cd /usr/customrepo && sudo -u customrepo -- make -s --debug=b --keep-going packages
 echo "Updating repository. This may take some time..."
-cd /usr/customrepo && sudo -u customrepo -- dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
+cd /usr/customrepo && sudo -u customrepo -- dpkg-scanpackages -m . /dev/null | gzip -9c > Packages.gz
 echo "Custom repository has been updated. Run apt update to check for package updates."
 
