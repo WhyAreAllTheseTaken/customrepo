605117be1a223393cc0cbfee1b44da93617afb54
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-09 12:58:32 +0100

    Maybe fix postinstall script.

--- gnat_postinst.sh
+++ gnat_postinst.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-/usr/share/gnat-deb/installer/ --script /usr/share/gnat-deb/installer/install-script.qs --platform minimal InstallPrefix="/opt/gnat-community"
+/usr/share/gnat-deb/installer --script /usr/share/gnat-deb/installer/install-script.qs --platform minimal InstallPrefix="/opt/gnat-community"
 
