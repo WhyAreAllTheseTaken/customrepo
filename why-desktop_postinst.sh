7c0d1abacc11858d940f8a3967be92e26d96292e
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-09 12:10:30 +0100

    Ignore errors if lightdm-gtk-greeter has not been run yet.

--- why-desktop_postinst.sh
+++ why-desktop_postinst.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-mv /etc/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf.old
+mv /etc/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf.old || true
 mv /etc/lightdm/lightdm-gtk-greeter.conf.why /etc/lightdm/lightdm-gtk-greeter.conf
 
--- why-desktop_prerm.sh
+++ why-desktop_prerm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 mv /etc/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf.why
-mv /etc/lightdm/lightdm-gtk-greeter.conf.old /etc/lightdm/lightdm-gtk-greeter.conf
+mv /etc/lightdm/lightdm-gtk-greeter.conf.old /etc/lightdm/lightdm-gtk-greeter.conf || true
 
