953bebcea02dc074918107cc998ac9de73acb5ef
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-09 13:11:55 +0100

    Add gnat to alternatives.

--- gnat_postinst.sh
+++ gnat_postinst.sh
@@ -2,3 +2,8 @@
 
 /usr/share/gnat-deb/installer --script /usr/share/gnat-deb/install-script.qs --platform minimal InstallPrefix="/opt/gnat-community"
 
+update-alternatives --install "/usr/bin/gnat" "gnat" "/opt/gnat-community/bin/gnat" 0
+update-alternatives --install "/usr/bin/gnatstudio" "gnatstudio" "/opt/gnat-community/bin/gnatstudio" 0
+update-alternatives --set gnat /opt/gnat-community/bin/gnat
+update-alternatives --set gnatstudio /opt/gnat-community/bin/gnatstudio
+
--- gnat_prerm.sh
+++ gnat_prerm.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 
+update-alternatives --remove "gnat" "/opt/gnat-community/bin/gnat"
+update-alternatives --remove "gnatstudio" "/opt/gnat-community/bin/gnatstudio"
+
 rm -rf /opt/gnat-community
 
