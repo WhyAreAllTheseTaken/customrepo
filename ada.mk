4b4bca0361c331c0c2c2de402d171da019832e95
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-10 14:51:33 +0100

    Don't redownload gnat-community every time.

--- ada.mk
+++ ada.mk
@@ -2,10 +2,13 @@ GNAT_VERSION = 2021.20210519
 GNAT_REVISION = 5
 GNAT_PACKAGE = gnat-community_$(GNAT_VERSION)-$(GNAT_REVISION)
 
-$(GNAT_PACKAGE).deb:
+gnat-community-installer_$(GNAT_VERSION):
+	wget -O gnat-community-installer_$(GNAT_VERSION) "https://community.download.adacore.com/v1/f3a99d283f7b3d07293b2e1d07de00e31e332325?filename=gnat-2021-20210519-x86_64-linux-bin&rand=1826"
+
+$(GNAT_PACKAGE).deb: gnat-community-installer_$(GNAT_VERSION)
 	rm -rf $(GNAT_PACKAGE)
 	mkdir -p $(GNAT_PACKAGE)/usr/share/gnat-deb/
-	wget -O $(GNAT_PACKAGE)/usr/share/gnat-deb/installer "https://community.download.adacore.com/v1/f3a99d283f7b3d07293b2e1d07de00e31e332325?filename=gnat-2021-20210519-x86_64-linux-bin&rand=1826"
+	cp gnat-community-installer_$(GNAT_VERSION) $(GNAT_PACKAGE)/usr/share/gnat-deb/installer
 	chmod +x $(GNAT_PACKAGE)/usr/share/gnat-deb/installer
 	wget -O $(GNAT_PACKAGE)/usr/share/gnat-deb/install-script.qs "https://raw.githubusercontent.com/AdaCore/gnat_community_install_script/refs/heads/master/install_script.qs"
 	chmod +x $(GNAT_PACKAGE)/usr/share/gnat-deb/install-script.qs
