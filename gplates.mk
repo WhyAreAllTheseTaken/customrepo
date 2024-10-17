d36901f26dc3736e13ebe28845c3aa8fabb10fe9
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-17 17:10:21 +0100

    Fix gplates source version.

--- gplates.mk
+++ gplates.mk
@@ -1,10 +1,11 @@
+GPLATES_SRC_VERSION = 2.5
 GPLATES_VERSION = 2.5.0
 GPLATES_PACKAGE = gplates_$(GPLATES_VERSION)_amd64
-GPLATES_DIR = gplates_$(GPLATES_VERSION)
+GPLATES_DIR = gplates_$(GPLATES_SRC_VERSION)
 
 $(GPLATES_DIR)/BUILD.Linux:
 	rm -rf $(GPLATES_DIR)
-	git clone --depth 1 --branch GPlates-$(GPLATES_VERSION) "https://github.com/GPlates/GPlates.git" $(GPLATES_DIR)
+	git clone --depth 1 --branch GPlates-$(GPLATES_SRC_VERSION) "https://github.com/GPlates/GPlates.git" $(GPLATES_DIR)
 
 $(GPLATES_DIR)/bin/gplates: $(GPLATES_DIR)/BUILD.Linux
 	cd $(GPLATES_DIR) && cmake .
