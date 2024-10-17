27859fc8e78b54aace76c1e6f8982703158a43d5
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-10-17 21:21:51 +0100

    Maybe set contact?

--- gplates.mk
+++ gplates.mk
@@ -8,9 +8,9 @@ $(GPLATES_DIR)/BUILD.Linux:
 	git clone --depth 1 --branch GPlates-$(GPLATES_SRC_VERSION) "https://github.com/GPlates/GPlates.git" $(GPLATES_DIR)
 
 $(GPLATES_DIR)/bin/gplates: $(GPLATES_DIR)/BUILD.Linux
-	cd $(GPLATES_DIR) && cmake .
+	cd $(GPLATES_DIR) && cmake -DCMAKE_INSTALL_PREFIX:STRING=/usr -DGPLATES_PACKAGE_CONTACT=WHyAreAllTheseTaken .
 	cd $(GPLATES_DIR) && make --debug=n
 
 $(GPLATES_PACKAGE).deb: $(GPLATES_DIR)/bin/gplates
-	cd $(GPLATES_DIR) && GPLATES_PACKAGE_CONTACT="WhyAreAllTheseTaken" make --debug=n package
+	cd $(GPLATES_DIR) && make --debug=n package
 
