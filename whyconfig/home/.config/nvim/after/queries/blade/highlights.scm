351316966852b552c5af6fa4bed9cac4e17503c9
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-11-07 10:28:52 +0000

    Configure blade syntax highlighting.

---
+++ whyconfig/home/.config/nvim/after/queries/blade/highlights.scm
@@ -0,0 +1,8 @@
+(directive) @function
+(directive_start) @function
+(directive_end) @function
+(comment) @comment
+((parameter) @include (#set! "priority" 110)) 
+((php_only) @include (#set! "priority" 110)) 
+(keyword) @keyworld
+
---
+++ whyconfig/home/.config/nvim/after/queries/blade/injections.scm
@@ -0,0 +1,7 @@
+((text) @injection.content
+   (#not-has-ancestor? @injection.content "envoy")
+   (#set! injection.combined)
+   (#set! injection.language php))
+((php_only) @injection.content
+   (#set! injection.language php_only))
+
