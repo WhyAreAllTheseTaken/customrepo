38d9eb61306221f261fdebda6830d3e05d2c137b
48136416+WhyAreAllTheseTaken@users.noreply.github.com <lucy@main.why>
2024-12-18 00:47:10 +0000

    Copy why-desktop configuration files on setup.

--- why-desktop_postinst.sh
+++ why-desktop_postinst.sh
@@ -5,3 +5,23 @@ if test -f /etc/lightdm/lightdm-gtk-greeter.conf; then
 fi
 mv /etc/lightdm/lightdm-gtk-greeter.conf.why /etc/lightdm/lightdm-gtk-greeter.conf
 
+for dir in /home/*/
+do
+    dir=${dir%*/}
+    user=${dir##*/}
+    mkdir -p /home/$user/.config/dunst/
+    cp /etc/skel/.config/dunst/dunstrc /home/$user/.config/dunst/dunstrc
+    mkdir -p /home/$user/.config/i3/
+    cp /etc/skel/.config/i3/config /home/$user/.config/i3/config
+    mkdir -p /home/$user/.config/i3blocks/
+    cp /etc/skel/.config/i3blocks/i3blocks.conf /home/$user/.config/i3blocks/i3blocks.conf
+    mkdir -p /home/$user/rofi/themes/
+    cp /etc/skel/.config/rofi/config.rasi /home/$user/.config/rofi/config.rasi
+    cp /etc/skel/.config/rofi/themes/Ice.rasi /home/$user/.config/rofi/themes/Ice.rasi
+    cp /etc/skel/.config/betterlockscreenrc /home/$user/.config/betterlockscreenrc
+    cp /etc/skel/.config/picom.conf /home/$user/.config/picom.conf
+    mkdir -p /home/$user/.config/flameshot
+    cp /etc/skel/.config/flameshot/flameshot.ini /home/$user/.config/flameshot/flameshot.ini
+done
+
+
