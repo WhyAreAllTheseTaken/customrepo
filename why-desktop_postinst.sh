905541f393b47d68398665d9716c9adf76fedd20
48136416+WhyAreAllTheseTaken@users.noreply.github.com <lucy@main.why>
2024-12-18 00:52:24 +0000

    Set the correct owner for config files.

--- why-desktop_postinst.sh
+++ why-desktop_postinst.sh
@@ -11,17 +11,25 @@ do
     user=${dir##*/}
     mkdir -p /home/$user/.config/dunst/
     cp /etc/skel/.config/dunst/dunstrc /home/$user/.config/dunst/dunstrc
+    chown $user:$user /home/$user/.config/dunst/dunstrc
     mkdir -p /home/$user/.config/i3/
     cp /etc/skel/.config/i3/config /home/$user/.config/i3/config
+    chown $user:$user /home/$user/.config/i3/config
     mkdir -p /home/$user/.config/i3blocks/
     cp /etc/skel/.config/i3blocks/i3blocks.conf /home/$user/.config/i3blocks/i3blocks.conf
+    chown $user:$user /home/$user/.config/i3blocks/i3blocks.conf
     mkdir -p /home/$user/.config/rofi/themes/
     cp /etc/skel/.config/rofi/config.rasi /home/$user/.config/rofi/config.rasi
+    chown $user:$user /home/$user/.config/rofi/config.rasi
     cp /etc/skel/.config/rofi/themes/Ice.rasi /home/$user/.config/rofi/themes/Ice.rasi
+    chown $user:$user /home/$user/.config/rofi/themes/Ice.rasi
     cp /etc/skel/.config/betterlockscreenrc /home/$user/.config/betterlockscreenrc
+    chown $user:$user /home/$user/.config/betterlockscreenrc
     cp /etc/skel/.config/picom.conf /home/$user/.config/picom.conf
+    chown $user:$user /home/$user/.config/picom.conf
     mkdir -p /home/$user/.config/flameshot
     cp /etc/skel/.config/flameshot/flameshot.ini /home/$user/.config/flameshot/flameshot.ini
+    chown $user:$user /home/$user/.config/flameshot/flameshot.ini
 done
 
 
