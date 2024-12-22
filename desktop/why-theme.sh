a94fb4a9dcc3f5150509f10b9e9c138683d92890
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-12-22 13:25:16 +0000

    Allow theme reloading.

--- desktop/why-theme.sh
+++ desktop/why-theme.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/bash
 
-while getopts "l" option; do
+while getopts "lr" option; do
    case $option in
       l)
          echo "Available themes:"
          ls /usr/share/why-desktop/theme/
          exit;;
+      r)
+         echo "Resetting theme."
+         why-theme "$(<~/.local/why-theme)"
+         exit;;
    esac
 done
 
 echo "Using theme $1..."
+
+echo $1 > ~/.local/why-theme
+
 cp -rTf /usr/share/why-desktop/theme/$1/config/ ~/.config
 cp -rTf /usr/share/why-desktop/theme/$1/home/ ~
 
