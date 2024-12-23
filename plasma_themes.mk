174b33aab6deaa6fa847ab99960fc40d9a8fe542
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-12-23 23:37:51 +0000

    Fix window decorations.

--- plasma_themes.mk
+++ plasma_themes.mk
@@ -1,63 +1,3 @@
-WINGS_PLASMA_VERSION = 1.0.0
-WINGS_PLASMA_REVISION = 3
-WINGS_PLASMA_PACKAGE = wings-plasma-theme_$(WINGS_PLASMA_VERSION)-$(WINGS_PLASMA_REVISION)_all
-
-$(REPO)/$(WINGS_PLASMA_PACKAGE).deb:
-	echo "Packaging $(WINGS_PLASMA_PACKAGE)..."
-	rm -rf ./Wings-Plasma-Themes
-	git clone --depth 1 https://github.com/L4ki/Wings-Plasma-Themes.git
-	mkdir -p $(WINGS_PLASMA_PACKAGE)/usr/share/plasma/desktoptheme/
-	cp -r ./Wings-Plasma-Themes/Wings\ Plasma\ Themes/* $(WINGS_PLASMA_PACKAGE)/usr/share/plasma/desktoptheme/
-	cp -r ./Wings-Plasma-Themes/Wings\ Global\ Themes/* $(WINGS_PLASMA_PACKAGE)/usr/share/plasma/desktoptheme/
-	mkdir -p $(WINGS_PLASMA_PACKAGE)/usr/share/plasma/look-and-feel/
-	cp -r ./Wings-Plasma-Themes/Wings\ Splshscreens/* $(WINGS_PLASMA_PACKAGE)/usr/share/plasma/look-and-feel/
-	mkdir -p $(WINGS_PLASMA_PACKAGE)/usr/share/sddm/themes/
-	cp -r ./Wings-Plasma-Themes/Wings\ SDDM\ Themes/* $(WINGS_PLASMA_PACKAGE)/usr/share/sddm/themes/
-	mkdir -p $(WINGS_PLASMA_PACKAGE)/usr/share/aurorae/themes/
-	cp -r ./Wings-Plasma-Themes/Wings\ Window\ Decorations/* $(WINGS_PLASMA_PACKAGE)/usr/share/aurorae/themes/
-	mkdir -p $(WINGS_PLASMA_PACKAGE)/usr/share/color-schemes/
-	cp -r ./Wings-Plasma-Themes/Wings\ Color\ Schemes/* $(WINGS_PLASMA_PACKAGE)/usr/share/color-schemes/
-	mkdir -p $(WINGS_PLASMA_PACKAGE)/DEBIAN
-	cp wings-plasma_control $(WINGS_PLASMA_PACKAGE)/DEBIAN/control
-	dpkg-deb --build $(WINGS_PLASMA_PACKAGE)
-	mv $(WINGS_PLASMA_PACKAGE).deb $(REPO)
-	rm -rf Wings-Plasma-Themes
-	rm -rf $(WINGS_PLASMA_PACKAGE)
-
-WINGS_ICON_VERSION = 1.0.0
-WINGS_ICON_REVISION = 3
-WINGS_ICON_PACKAGE = wings-plasma-icon-theme_$(WINGS_ICON_VERSION)-$(WINGS_ICON_REVISION)_all
-
-$(REPO)/$(WINGS_ICON_PACKAGE).deb:
-	echo "Packaging $(WINGS_ICON_PACKAGE)..."
-	rm -rf ./Wings-Plasma-Themes
-	git clone --depth 1 https://github.com/L4ki/Wings-Plasma-Themes.git
-	mkdir -p $(WINGS_ICON_PACKAGE)/usr/share/icons/
-	cp -r ./Wings-Plasma-Themes/Wings\ Icons\ Themes/* $(WINGS_ICON_PACKAGE)/usr/share/icons/
-	mkdir -p $(WINGS_ICON_PACKAGE)/DEBIAN
-	cp wings-plasma-icon_control $(WINGS_ICON_PACKAGE)/DEBIAN/control
-	dpkg-deb --build $(WINGS_ICON_PACKAGE)
-	mv $(WINGS_ICON_PACKAGE).deb $(REPO)
-	rm -rf Wings-Plasma-Themes
-	rm -rf $(WINGS_ICON_PACKAGE)
-
-WINGS_WALLPAPER_VERSION = 1.0.0
-WINGS_WALLPAPER_REVISION = 3
-WINGS_WALLPAPER_PACKAGE = wings-plasma-wallpapers_$(WINGS_WALLPAPER_VERSION)-$(WINGS_WALLPAPER_REVISION)_all
-
-$(REPO)/$(WINGS_WALLPAPER_PACKAGE).deb:
-	echo "Packaging $(WINGS_WALLPAPER_PACKAGE)..."
-	rm -rf ./Wings-Plasma-Themes
-	git clone --depth 1 https://github.com/L4ki/Wings-Plasma-Themes.git
-	mkdir -p $(WINGS_WALLPAPER_PACKAGE)/usr/share/backgrounds/
-	cp -r ./Wings-Plasma-Themes/Wings\ Wallpapers/* $(WINGS_WALLPAPER_PACKAGE)/usr/share/backgrounds/
-	mkdir -p $(WINGS_WALLPAPER_PACKAGE)/DEBIAN
-	cp wings-plasma-wallpapers_control $(WINGS_WALLPAPER_PACKAGE)/DEBIAN/control
-	dpkg-deb --build $(WINGS_WALLPAPER_PACKAGE)
-	mv $(WINGS_WALLPAPER_PACKAGE).deb $(REPO)
-	rm -rf Wings-Plasma-Themes
-	rm -rf $(WINGS_WALLPAPER_PACKAGE)
-
 %-Plasma-Themes: %
 	rm -rf ./$@
 	git clone --depth 1 https://github.com/L4ki/$@.git
@@ -82,7 +22,12 @@ $(REPO)/$(WINGS_WALLPAPER_PACKAGE).deb:
 		cp -r ./$<-Plasma-Themes/$<\ SDDM\ Themes/* $@/usr/share/sddm/themes/;\
 	fi
 	mkdir -p $@/usr/share/aurorae/themes/
-	cp -r ./$<-Plasma-Themes/$<\ Window\ Decorations/* $@/usr/share/aurorae/themes/
+	if [[ -f $<-Plasma-Themes/$<\ Window\ Decorations ]]; then\
+		cp -r ./$<-Plasma-Themes/$<\ Window\ Decorations/* $@/usr/share/aurorae/themes/;\
+	fi
+	if [[ -f $<-Plasma-Themes/$<\ Windows\ Decorations ]]; then\
+		cp -r ./$<-Plasma-Themes/$<\ Windows\ Decorations/* $@/usr/share/aurorae/themes/;\
+	fi
 	mkdir -p $@/usr/share/color-schemes/
 	cp -r ./$<-Plasma-Themes/$<\ Color\ Schemes/* $@/usr/share/color-schemes/
 	mkdir -p $@/DEBIAN
@@ -152,7 +97,7 @@ $(REPO)/%-wallpapers.deb: %-wallpapers
 	dpkg-deb --build $<
 	mv $<.deb $(REPO)
 
-THEME_TARGETS=Infinity_themes
+THEME_TARGETS=Infinity_themes Wings_themes
 
 $(THEME_TARGETS):: %_themes: $(REPO)/%-plasma-theme.deb $(REPO)/%-icon-theme.deb $(REPO)/%-wallpapers.deb $(REPO)/%-gtk-theme.deb
 
