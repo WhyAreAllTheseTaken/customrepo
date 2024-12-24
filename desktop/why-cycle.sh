9a764ff8f7aec4263e0ff2912b9ee829ebac1f09
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-12-24 15:26:43 +0000

    Fix swap syntax.

--- desktop/why-cycle.sh
+++ desktop/why-cycle.sh
@@ -16,12 +16,12 @@ echo "Hour: $hour"
 
 case $hour in
     0 | 1 | 2 | 3 | 4 | 5)
-        swap_theme("pink");;
+        swap_theme pink;;
     6 | 7 | 8 | 9 | 10 | 11 | 12)
-        swap_theme("dawn");;
+        swap_theme dawn;;
     13 | 14 | 15 | 16 | 17 | 18)
-        swap_theme("ice");;
+        swap_theme ice;;
     19 | 20 | 21 | 22 | 23)
-        swap_theme("pink");;
+        swap_theme pink;;
 esac
 
