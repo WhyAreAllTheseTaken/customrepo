fe7cf13cf65b990cbefa1927ea46c3b60ce0c570
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-12-24 13:21:08 +0000

    Remove azure theme.

--- plasma_themes.mk
+++ plasma_themes.mk
@@ -135,7 +135,7 @@ $(REPO)/%-wallpapers_all.deb: %-wallpapers_all
 	mv $<.deb $(REPO)
 
 THEME_TARGETS=Infinity_themes Wings_themes Relax_themes Cool_themes Gradient_themes Flight_themes Vivid_themes Colorful_themes \
-			  Magna_themes Gently-Color_themes Bluish_themes Silvery_themes Viola_themes Vortex_themes Azure_themes Aretha_themes \
+			  Magna_themes Gently-Color_themes Bluish_themes Silvery_themes Viola_themes Vortex_themes Aretha_themes \
               Pax_themes Shadows_themes Fancy_themes Desert_themes Bonny_themes Magna_themes
 
 $(THEME_TARGETS):: %_themes: $(REPO)/%-kde_all.deb $(REPO)/%-icon-theme_all.deb \
