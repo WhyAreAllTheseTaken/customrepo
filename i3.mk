43e2629c8554d737603e82a69c30f57da3fbedfe
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-12-26 15:49:13 +0000

    Fix i3blocks.

--- i3.mk
+++ i3.mk
@@ -2,6 +2,7 @@ I3_BLOCKS_PACKAGE = i3blocks-contrib_all
 
 $(I3_BLOCKS_PACKAGE): i3_blocks_control
 	echo "Packaging $@..."
+	rm -rf $@
 	rm -rf ./i3blocks-contrib
 	git clone --depth 1 --branch v2.0.0 https://github.com/vivien/i3blocks-contrib.git
 	mkdir -p $@/etc/skel/.config/i3blocks/
