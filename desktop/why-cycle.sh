75adcf8e2bb9cb3147344f91a50988b50d49c250
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-12-26 16:26:43 +0000

    Fix theme checking not working.

--- desktop/why-cycle.sh
+++ desktop/why-cycle.sh
@@ -81,7 +81,7 @@ while getopts "lrhynsd" option; do
             done
             exit;;
         s)
-            if [[ -f /usr/share/why-desktop/theme/$2 ]]; then
+            if [[ -d /usr/share/why-desktop/theme/$2 ]]; then
                 echo $3 > ~/.config/why-cycle/$2
             else
                 echo "The theme '$3' does not exist." >&2
