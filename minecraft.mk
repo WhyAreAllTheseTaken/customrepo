ead3b0e156538071940314e278ceb73575ad186b
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-12-26 20:56:30 +0000

    Add multimc.

--- minecraft.mk
+++ minecraft.mk
@@ -4,6 +4,12 @@ MINECRAFT_PACKAGE = minecraft_$(MINECRAFT_VERSION)-$(MINECRAFT_REVISION)_all
 
 $(REPO)/$(MINECRAFT_PACKAGE).deb:
 	echo "Downloading $(MINECRAFT_PACKAGE)..."
-	wget https://launcher.mojang.com/download/Minecraft.deb -O $(REPO)/$(MINECRAFT_PACKAGE).deb
+	wget https://launcher.mojang.com/download/Minecraft.deb -O $@
 
+MULTIMC_VERSION = 1.6-1
+MULTIMC_PACKAGE = multimc_$(MULTIMC_VERSION)_amd64
+
+$(REPO)/multimc_%_amd64.deb:
+	echo "Downloading $(MULTIMC_PACKAGE)..."
+	wget https://files.multimc.org/downloads/multimc_%.deb -O $@
 
