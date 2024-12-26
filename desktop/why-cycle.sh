b8ef7505d5aeb78fc085936761392223a7983287
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-12-26 16:20:43 +0000

    Add customisable theme cycles.

--- desktop/why-cycle.sh
+++ desktop/why-cycle.sh
@@ -10,46 +10,117 @@ function swap_theme() {
     fi
 }
 
-while getopts "rhed" option; do
+function init_theme() {
+    if [[ ! -f ~/.config/why-cycle/$1 ]]; then
+        echo "$2" > ~/.config/why-cycle/$1
+    fi
+}
+
+mkdir -p ~/.config/why-cycle
+init_theme 0 night
+init_theme 1 night
+init_theme 2 night
+init_theme 3 night
+init_theme 4 night
+init_theme 5 night
+init_theme 6 dawn
+init_theme 7 dawn
+init_theme 8 dawn
+init_theme 9 dawn
+init_theme 10 dawn
+init_theme 11 ice
+init_theme 12 ice
+init_theme 13 ice
+init_theme 14 ice
+init_theme 15 ice
+init_theme 16 ice
+init_theme 17 ice
+init_theme 18 ice
+init_theme 19 pink
+init_theme 20 pink
+init_theme 21 pink
+init_theme 22 pink
+init_theme 23 night
+
+while getopts "lrhynsd" option; do
     case $option in
+        d)
+            mkdir -p ~/.config/why-cycle
+            echo night > ~/.config/why-cycle/0
+            echo night > ~/.config/why-cycle/1
+            echo night > ~/.config/why-cycle/2
+            echo night > ~/.config/why-cycle/3
+            echo night > ~/.config/why-cycle/4
+            echo night > ~/.config/why-cycle/5
+            echo dawn > ~/.config/why-cycle/6
+            echo dawn > ~/.config/why-cycle/7
+            echo dawn > ~/.config/why-cycle/8
+            echo dawn > ~/.config/why-cycle/9
+            echo dawn > ~/.config/why-cycle/10
+            echo ice > ~/.config/why-cycle/11
+            echo ice > ~/.config/why-cycle/12
+            echo ice > ~/.config/why-cycle/13
+            echo ice > ~/.config/why-cycle/14
+            echo ice > ~/.config/why-cycle/15
+            echo ice > ~/.config/why-cycle/16
+            echo ice > ~/.config/why-cycle/17
+            echo ice > ~/.config/why-cycle/18
+            echo pink > ~/.config/why-cycle/19
+            echo pink > ~/.config/why-cycle/20
+            echo pink > ~/.config/why-cycle/21
+            echo pink > ~/.config/why-cycle/22
+            echo night > ~/.config/why-cycle/23
+            exit;;
+        l)
+            for hour in $(seq 0 23);
+            do
+                path=~/.config/why-cycle/$hour
+                theme=$(cat $path)
+
+                echo "$hour: $theme"
+            done
+            exit;;
+        s)
+            if [[ -f /usr/share/why-desktop/theme/$2 ]]; then
+                echo $3 > ~/.config/why-cycle/$2
+            exit;;
+            fi
+                echo "The specified theme does not exist." >&2
+            exit;;
         r)
             echo "Refreshing theme..."
-            if [[ -f ~/.config/why-cycle-enable ]]; then
+
+            if [[ -f ~/.config/why-cycle/enabled ]]; then
                 hour=$(date +"%H")
 
                 echo "Hour: $hour"
 
-                case $hour in
-                    0 | 1 | 2 | 3 | 4 | 5)
-                        swap_theme night;;
-                    6 | 7 | 8 | 9 | 10 | 11)
-                        swap_theme dawn;;
-                    12 | 13 | 14 | 15 | 16 | 17 | 18)
-                        swap_theme ice;;
-                    19 | 20 | 21)
-                        swap_theme pink;;
-                    22 | 23)
-                        swap_theme night;;
-                esac
+                path=~/.config/why-cycle/$hour
+                swap_theme $(cat $path)
             else
                 echo "why-cycle is not enabled by this user."
             fi
             exit;;
         h)
-            echo "Available options:"
+            echo "Available Commands:"
             echo "-r - Checks to see if a new theme should be set."
+            echo "-s <hour> <theme> - Sets the theme for the specified hour."
+            echo "-l - Lists what theme is used for each hour."
             echo "-h - Displays this help page."
-            echo "-e - Enables cycle themes for this user."
-            echo "-d - Disables cycle themes for this user."
+            echo "-y - Enables cycle themes for this user."
+            echo "-n - Disables cycle themes for this user."
+            echo "-d - Reset to default options."
             exit;;
-        e)
+        y)
             echo "Enabling theme cycling..."
-            touch ~/.config/why-cycle-enable
+            mkdir -p ~/.config/why-cycle
+            touch ~/.config/why-cycle/enabled
             why-cycle -r
             exit;;
-        d)
+        n)
             echo "Disabling theme cycling..."
-            rm ~/.config/why-cycle-enable
+            mkdir -p ~/.config/why-cycle
+            rm ~/.config/why-cycle/enabled
             exit;;
     esac
 done
