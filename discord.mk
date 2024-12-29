d6b05921eab372b9e1b469dc2f7593937c58971a
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-12-29 21:37:19 +0000

    Pre-empt discord versions.

--- discord.mk
+++ discord.mk
@@ -1,7 +1,13 @@
-DISCORD_VERSION = 0.0.78
-DISCORD_PACKAGE = discord_$(DISCORD_VERSION)_amd64
-
 $(REPO)/discord_%_amd64.deb:
 	echo "Downloading $@..."
-	wget --show-progress -O $@ https://stable.dl2.discordapp.net/apps/linux/$*/discord-$*.deb
+	-wget --show-progress -O $@ https://stable.dl2.discordapp.net/apps/linux/$*/discord-$*.deb
+
+discord_versions: $(REPO)/discord_0.0.78_amd64.deb \
+	$(REPO)/discord_0.0.79_amd64.deb \
+	$(REPO)/discord_0.0.80_amd64.deb \
+	$(REPO)/discord_0.0.81_amd64.deb \
+	$(REPO)/discord_0.0.82_amd64.deb \
+	$(REPO)/discord_0.0.83_amd64.deb \
+	$(REPO)/discord_0.0.84_amd64.deb \
+	$(REPO)/discord_0.0.85_amd64.deb
 
