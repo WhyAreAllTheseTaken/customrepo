25d4836dcbac907474db8a3facd591ff5e5321f7
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-12-31 00:24:57 +0000

    Remove old discord versions.

--- discord.mk
+++ discord.mk
@@ -1,6 +1,9 @@
 $(REPO)/discord_%_amd64.deb:
 	echo "Downloading $@..."
 	-wget --show-progress -O $@ https://stable.dl2.discordapp.net/apps/linux/$*/discord-$*.deb
+	if [[ ! -s $@ ]]; then\
+		rm $@;\
+	fi
 
 discord_versions: $(REPO)/discord_0.0.78_amd64.deb \
 	$(REPO)/discord_0.0.79_amd64.deb \
