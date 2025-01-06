d03b43fc7004331640197f8f844adfe113d47f80
WhyAreAllTheseTaken <48136416+WhyAreAllTheseTaken@users.noreply.github.com>
2025-01-06 22:45:45 +0000

    Url encode.

--- web.mk
+++ web.mk
@@ -4,11 +4,11 @@
 	mkdir -p $@/usr/bin/
 	cp web.sh $@/usr/bin/$<-web
 	chmod +x $@/usr/bin/$<-web
-	sed -i "s/%url%/$$(cat $<)/g" $@/usr/bin/$<-web
+	sed -i "s/%url%/$$(cat $<//\//\\\//)/g" $@/usr/bin/$<-web
 	mkdir -p $@/DEBIAN
-	cp desktop/web/control $@/DEBIAN/control
+	cp web_control $@/DEBIAN/control
 	sed -i "s/%name%/\l$</g" $@/DEBIAN/control
-	sed -i "s/%url%/$$(cat $<)/g" $@/DEBIAN/control
+	sed -i "s/%url%/$$(cat $<//\//\\\//)/g" $@/DEBIAN/control
 
 web_sites: repo/github-web_all.deb repo/soundcloud-web_all.deb repo/todoist-web_all.deb repo/toggl-track-web_all.deb
 
