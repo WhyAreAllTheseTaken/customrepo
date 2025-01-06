8826c5783ea1d34a5af2270bf0f5ef023757de3c
WhyAreAllTheseTaken <48136416+WhyAreAllTheseTaken@users.noreply.github.com>
2025-01-06 22:56:08 +0000

    Use a different sed char.

--- web.mk
+++ web.mk
@@ -4,11 +4,11 @@
 	mkdir -p $@/usr/bin/
 	cp web.sh $@/usr/bin/$<-web
 	chmod +x $@/usr/bin/$<-web
-	sed -i "s/%url%/$$(cat $< | sed "s/\//\\\//g")/g" $@/usr/bin/$<-web
+	sed -i "s=%url%=$$(cat $<)=g" $@/usr/bin/$<-web
 	mkdir -p $@/DEBIAN
 	cp web_control $@/DEBIAN/control
 	sed -i "s/%name%/\l$</g" $@/DEBIAN/control
-	sed -i "s/%url%/$$(cat $< | sed "s/\//\\\//g")/g" $@/DEBIAN/control
+	sed -i "s=%url%=$$(cat $<)=g" $@/DEBIAN/control
 
 web_sites: repo/github-web_all.deb repo/soundcloud-web_all.deb repo/todoist-web_all.deb repo/toggl-track-web_all.deb
 
