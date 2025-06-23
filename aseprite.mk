12c2c12f9838b458f2e5f8710ac92083255812f9
WhyAreAllTheseTaken <48136416+WhyAreAllTheseTaken@users.noreply.github.com>
2025-06-23 14:09:51 +0100

    Use a release build of skia.

--- aseprite.mk
+++ aseprite.mk
@@ -1,9 +1,9 @@
 ASEPRITE_VERSION=1.3.14
 
-skia.zip:
-	wget --show-progress -O $@ "https://github.com/aseprite/skia/releases/download/m124-08a5439a6b/Skia-Linux-Debug-x64.zip"
+skia-release.zip:
+	wget --show-progress -O $@ "https://github.com/aseprite/skia/releases/download/m124-08a5439a6b/Skia-Linux-Release-x64.zip"
 
-skia: skia.zip
+skia-release: skia-release.zip
 	mkdir -p $@
 	unzip $< -d $@
 
@@ -11,7 +11,7 @@ aseprite:
 	git clone --depth 1 --branch v$(ASEPRITE_VERSION) https://github.com/aseprite/aseprite.git
 	cd aseprite && git submodule update --init --recursive
 
-aseprite_done: aseprite skia
+aseprite_done: aseprite skia-release
 	echo "Compiling $@..."
 	rm -rf $@
 	mkdir $@
@@ -21,10 +21,10 @@ aseprite_done: aseprite skia
 	  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
 	  -DCMAKE_CXX_FLAGS:STRING=-stdlib=libc++ \
 	  -DCMAKE_EXE_LINKER_FLAGS:STRING=-stdlib=libc++ \
-	  -DLAF_BACKEND=skia \
-	  -DSKIA_DIR=/usr/customrepo/skia \
-	  -DSKIA_LIBRARY_DIR=/usr/customrepo/skia/out/Release-x64 \
-	  -DSKIA_LIBRARY=/usr/customrepo/skia/out/Release-x64/libskia.a \
+	  -DLAF_BACKEND=skia-release \
+	  -DSKIA_DIR=/usr/customrepo/skia-release \
+	  -DSKIA_LIBRARY_DIR=/usr/customrepo/skia-release/out/Release-x64 \
+	  -DSKIA_LIBRARY=/usr/customrepo/skia-release/out/Release-x64/libskia.a \
 	  -G Ninja \
 	  ..
 	export CC=clang; \
