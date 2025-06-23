7564e10a04b80f0ccb08b0af2e6ead228efc5740
WhyAreAllTheseTaken <48136416+WhyAreAllTheseTaken@users.noreply.github.com>
2025-06-23 13:38:28 +0100

    Fix skia.

--- aseprite.mk
+++ aseprite.mk
@@ -5,19 +5,24 @@ depot_tools:
 
 skia:
 	git clone --depth 1 --branch aseprite-m102 https://github.com/aseprite/skia.git
+
+skia_done: skia
+	echo "Compiling skia..."
 	export CC=clang; \
 	export CXX=clang++; \
-	export PATH="${PWD}/depot_tools:${PATH}"; \
+	export PATH="$${PWD}/depot_tools:$${PATH}"; \
+	echo $$PATH; \
 	cd skia; \
 	python3 tools/git-sync-deps; \
 	gn gen out/Release-x64 --args='is_debug=false is_official_build=true skia_use_system_expat=false skia_use_system_icu=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false skia_use_sfntly=false skia_use_freetype=true skia_use_harfbuzz=true skia_pdf_subset_harfbuzz=true skia_use_system_freetype2=false skia_use_system_harfbuzz=false cc="clang" cxx="clang++" extra_cflags_cc=["-stdlib=libc++"] extra_ldflags=["-stdlib=libc++"]'; \
 	ninja -C out/Release-x64 skia modules
+	touch skia_done
 
 aseprite:
 	git clone --depth 1 --branch v$(ASEPRITE_VERSION) https://github.com/aseprite/aseprite.git
 	cd aseprite && git submodule update --init --recursive
 
-aseprite/build: aseprite skia
+aseprite/build: aseprite skia_done
 	echo "Compiling $@..."
 	rm -rf $@
 	mkdir $@
