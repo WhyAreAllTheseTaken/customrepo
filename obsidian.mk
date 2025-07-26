6b674afa35d564429041f9bb5b5e56e768042e05
WhyAreAllTheseTaken <48136416+WhyAreAllTheseTaken@users.noreply.github.com>
2025-07-26 13:11:12 +0100

    Maybe fix obsidian.

--- obsidian.mk
+++ obsidian.mk
@@ -3,5 +3,4 @@ OBSIDIAN_PACKAGE = obsidian_$(OBSIDIAN_VERSION)_amd64
 
 $(REPO)/obsidian_%_amd64.deb:
 	echo "Downloading $@..."
-	wget --show-progress -O $@ https://github.com/obsidianmd/obsidian-releases/releases/download/v$*/obsidian_$@_amd64.deb
-
+	wget --show-progress -O $@ https://github.com/obsidianmd/obsidian-releases/releases/download/v$*/obsidian_$*_amd64.deb
