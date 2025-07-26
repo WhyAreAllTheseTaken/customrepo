aff6ad2613b748f5b5c3091a26438d38b5be739a
WhyAreAllTheseTaken <48136416+WhyAreAllTheseTaken@users.noreply.github.com>
2025-07-26 16:41:09 +0100

    Fix issue where the desktop is installed with the wrong permissions.

--- desktop/why-desktop_postinst.sh
+++ desktop/why-desktop_postinst.sh
@@ -11,15 +11,17 @@ for dir in /home/*/
 do
     dir=${dir%*/}
     user=${dir##*/}
+    mkdir -p /home/$user/.config/
+    chown $user:$user /home/$user/.config
     mkdir -p /home/$user/.config/i3/
     cp /etc/skel/.config/i3/config /home/$user/.config/i3/config
-    chown $user:$user /home/$user/.config/i3/config
+    chown -R $user:$user /home/$user/.config/i3
     mkdir -p /home/$user/.config/i3blocks/
     cp /etc/skel/.config/i3blocks/i3blocks.conf /home/$user/.config/i3blocks/i3blocks.conf
-    chown $user:$user /home/$user/.config/i3blocks/i3blocks.conf
+    chown -R $user:$user /home/$user/.config/i3blocks
     mkdir -p /home/$user/.config/betterlockscreen
     cp /etc/skel/.config/betterlockscreen/betterlockscreenrc /home/$user/.config/betterlockscreen/betterlockscreenrc
-    chown $user:$user /home/$user/.config/betterlockscreen/betterlockscreenrc
+    chown -R $user:$user /home/$user/.config/betterlockscreen
     cp /etc/skel/.config/picom.conf /home/$user/.config/picom.conf
     chown $user:$user /home/$user/.config/picom.conf
 done
