58cc368c622d8c2a01bcf81298364a90b63beb02
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2026-01-27 15:28:24 +0000

    Update NTSYNC package to Debian 13.

--- ntsync.mk
+++ ntsync.mk
@@ -4,7 +4,7 @@ morgwai-ntsync_all: ntsync_control
 	mkdir -p $@/etc/apt/keyrings/ 
 	wget -O $@/etc/apt/keyrings/morgwai-obs.gpg https://build.opensuse.org/projects/home:morgwai/signing_keys/download?kind=gpg
 	mkdir -p $@/etc/apt/sources.list.d/ 
-	echo "deb [signed-by=/etc/apt/keyrings/morgwai-obs.gpg] http://download.opensuse.org/repositories/home:/morgwai:/ntsync/Debian_Testing /" > $@/etc/apt/sources.list.d/wine-ntsync.list
+	echo "deb [signed-by=/etc/apt/keyrings/morgwai-obs.gpg] http://download.opensuse.org/repositories/home:/morgwai:/ntsync/Debian_13 /" > $@/etc/apt/sources.list.d/wine-ntsync.list
 	mkdir -p $@/DEBIAN
 	cp $< $@/DEBIAN/control
 
