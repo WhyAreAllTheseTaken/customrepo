f699164780a2aa8f7c7f645234ab918f2badb19e
WhyAreAllTheseTaken <48136416+WhyAreAllTheseTaken@users.noreply.github.com>
2026-02-22 22:03:26 +0000

    Update deps.

---
+++ desktop/why-cycle_install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+post_install() {
+    for dir in /home/*/
+    do
+        dir=${dir%*/}
+        user=${dir##*/}
+        systemctl -M "$user@" --user daemon-reload
+        systemctl -M "$user@" --user start why-cycle.service
+    done
+    systemctl --global enable why-cycle.timer
+}
+
+post_remove() {
+    for dir in /home/*/
+    do
+        dir=${dir%*/}
+        user=${dir##*/}
+        systemctl -M "$user@" --user daemon-reload
+    done
+}
+
+pre_remove() {
+    systemctl --global disable why-cycle.timer
+}
